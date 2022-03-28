f30895cab30e656a879efee6b98f5cca5b658aca
avillalbacantero <villalbacanteroa@gmail.com>
2022-03-28 20:36:35 +0200

    👌 IMPROVE: Add more logging and write the output preprocessed dataset.

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -24,6 +24,7 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df["Name"] = df["Name"].apply(
         lambda x: x.translate(str.maketrans(" ", " ", string.punctuation))
     )
+    logging.debug("Cleaned `Name` column.")
 
     nltk.download("vader_lexicon")  # downloads lexicon if not exists
     sid = SentimentIntensityAnalyzer()
@@ -33,6 +34,7 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df["Negative Sentiment"] = df.Sentiment.apply(lambda x: x["neg"])
     df["Compound Sentiment"] = df.Sentiment.apply(lambda x: x["compound"])
     df = df.drop(columns=["Sentiment"])
+    logging.debug("Sentiment and Compound sentiment have been calculated over the `Name` column.")
 
     return df
 
@@ -45,9 +47,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process the dataset.")
     parser.add_argument("--input", type=str, help="Path/to the input CSV to process.")
     args = parser.parse_args()
-
-    df = pd.read_csv(args.input)
     
+    logging.info(f"Preprocessing {args.input}")
+    df = pd.read_csv(args.input)
+
     # DVC needs that the script creates the output directory
     OUTPUT_DIR = "./data/preprocessed"
     if not os.path.exists(OUTPUT_DIR):
@@ -56,3 +59,8 @@ if __name__ == "__main__":
     logging.info(f"Preprocessing: {args.input}")
     preprocessed_df = preprocess(df)
     logging.info(f"Preprocessed: {args.input}")
+
+    # Write intermediate result
+    output_file = os.path.join(OUTPUT_DIR, "preprocessed_dataset.csv")
+    preprocessed_df.to_csv(output_file, index=False)
+    logging.info(f"Preprocessed dataset written to: {output_file}")
