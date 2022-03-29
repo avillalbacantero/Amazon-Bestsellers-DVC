8bf50c8f99ee9b0ff2e9c0593b513ede9eb37709
avillalbacantero <villalbacanteroa@gmail.com>
2022-03-29 20:11:54 +0200

    👌 IMPROVE: Add new featurization steps

--- src/features/featurize.py
+++ src/features/featurize.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+from sklearn.preprocessing import LabelEncoder
 import yaml
 
 import numpy as np
@@ -11,22 +12,37 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 
 
-def featurize(df: pd.DataFrame, num_components: int) -> np.ndarray:
-    """Featurizes input data.
+def encode_genre(df: pd.DataFrame) -> pd.DataFrame:
+    """Encodes the `Genre` columns using values between 0 and
+    `num_classes -1`.
 
     Args:
-        df (pd.DataFrame): The input DataFrame to featurize.
+        df (pd.DataFrame): Original DataFrame.
 
     Returns:
-        pd.DataFrame: The featurized DataFrame.
+        pd.DataFrame: Encoded DataFrame
+    """
+    label_encoder = LabelEncoder()
+    df["Genre"] = label_encoder.fit_transform(df["Genre"])
+    return df
+
+
+def featurize_name(name_series: pd.Series, num_components: int) -> np.ndarray:
+    """Featurizes the `Name` of the books data.
+
+    Args:
+        df (pd.Series): The Series of `Name` to featurize.
+
+    Returns:
+        np.ndarray: The name features
     """
 
-    # Apply TF-IDF feature extraction
+    # Apply TF-IDF feature extraction to the `Name` column
     vectorizer = TfidfVectorizer()
-    sp_matrix = vectorizer.fit_transform(df["Name"])
+    sp_matrix = vectorizer.fit_transform(name_series)
     logging.debug("Performed Feature Extraction using TD-IDF.")
 
-    # Apply feature decomposition
+    # Apply feature decomposition to those features
     svd_truncer = TruncatedSVD(n_components=num_components, random_state=1)
     dec_mat = svd_truncer.fit_transform(sp_matrix)
     logging.debug("Performed Feature Decomposition using Truncated SVD.")
@@ -44,7 +60,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process the dataset.")
     parser.add_argument("--input", type=str, help="Path/to the input CSV to featurize.")
     args = parser.parse_args()
-    
+
     logging.info(f"Featurizing {args.input}")
 
     # Read the parameters to execute featurization
@@ -58,11 +74,37 @@ if __name__ == "__main__":
     if not os.path.exists(OUTPUT_DIR):
         os.mkdir(OUTPUT_DIR)
 
-    logging.info(f"Extracting features from: {args.input}")
-    featurized_dataset = featurize(df, num_components=params["truncatedSVD_number_of_components"])
-    logging.info(f"Features extracted: {args.input}")
+    # Feature extraction
+    logging.info(f"Extracting name features from: {args.input}")
+    featurized_name = featurize_name(
+        df["Name"], num_components=params["truncatedSVD_number_of_components"]
+    )
+    logging.info(f"Name features extracted: {args.input}")
+
+    logging.info(f"Encoding data from: {args.input}")
+    df_label = df["Price"]
+    df = df.drop(columns=["Price"])
+    encoded_df = encode_genre(df)
+    logging.info(f"Data has been encoded: {args.input}")
+
+    # Drop the columns the solution does not use
+    encoded_df = encoded_df.drop(
+        columns=[
+            "Name",
+            "Author",
+            "User Rating",
+            "Reviews",
+            "Positive Sentiment",
+            "Neutral Sentiment",
+            "Negative Sentiment",
+        ]
+    )
+
+    # Concatenate the features
+    data_features = encoded_df.to_numpy()
+    data_features = np.hstack((featurized_name, data_features))
 
     # Write intermediate result
     output_file = os.path.join(OUTPUT_DIR, "featurized_dataset")
-    np.save(output_file, featurized_dataset)
+    np.savez_compressed(output_file, data_features=data_features, label=df_label.values)
     logging.info(f"Featurized dataset written to: {output_file}")
